73322d710191e9777013d365144255e42f74a0ef
pduleba <pduleba@pgs-soft.com>
2019-04-16 16:05:46 +0200

    Exercise 01 - Start Jenkins inside Docker

--- exercise-01/run-jenkins.bat
+++ exercise-01/run-jenkins.bat
@@ -1,1 +1,1 @@
-docker run --rm -p 8080:8080 -p 50000:50000 --name jenkins -v C:/work-pduleba/programs/jenkins:/var/jenkins_home jenkins/jenkins:lts
+docker run -p 8080:8080 -p 50000:50000 --name jenkins -v C:/work-pduleba/programs/jenkins:/var/jenkins_home jenkins/jenkins:lts
